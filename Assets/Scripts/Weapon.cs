4836120c93cfbcb62e677b9484df90c4e616f7b8
Chicken_cyborg <155845704+joaocostaIpca@users.noreply.github.com>
2025-09-25 12:06:31 +0100

    Bullet system revamped
    Add Bullet counter with max ammo and max magazine ammo, added a cooldown to reload and shoting , revamped bullet model

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,26 +1,79 @@
 using UnityEngine;
-
+using TMPro;
 public class Weapon : MonoBehaviour
 {
     public GameObject bulletPrefab;   // Prefab do projetil
     public Transform firePoint;       // Ponto de disparo
     public float bulletSpeed = 20f;
 
+    public int maxMagAmmo;
+    public int maxAmmo;
+    private int currentAmmo;
+
+
+    public float fireRate = 0.5f;     // Intervalo entre tiros
+    public float reloadCooldown = 5f; // Intervalo entre reloads
+    private float nextFireTime = 0f;
+    private float nextReloadTime = 0f;
+
+    public TextMeshProUGUI ammoText;
+    void Start()
+    {
+        currentAmmo = maxMagAmmo;
+        UpdateAmmoUI();
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Botão esquerdo do rato
+       if (Input.GetMouseButtonDown(0) && Time.time >= nextFireTime) // Botão esquerdo do rato
         {
             Shoot();
         }
+
+         if (Input.GetKeyDown(KeyCode.R) && Time.time >= nextReloadTime) // Tecla R para recarregar
+        {
+            reload();
+        }
+    }
+
+    void reload()
+    {
+        int ammoNeeded = maxMagAmmo - currentAmmo;
+        if (maxAmmo >= ammoNeeded)
+        {
+            maxAmmo -= ammoNeeded;
+            currentAmmo = maxMagAmmo;
+        }
+        else
+        {
+            currentAmmo += maxAmmo;
+            maxAmmo = 0;
+        }
+        
+        // Definir cooldown do reload
+        nextReloadTime = Time.time + reloadCooldown;
+        UpdateAmmoUI();
     }
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.linearVelocity = firePoint.forward * bulletSpeed;
+        if (currentAmmo > 0)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.linearVelocity = firePoint.forward * bulletSpeed;
 
-        // Destruir a bala após 3 segundos para não encher a cena
-        Destroy(bullet, 3f);
+            // Destruir a bala após 1.5 segundos para não encher a cena
+            Destroy(bullet, 1.5f);
+            currentAmmo--;
+            // Definir cooldown do tiro
+            nextFireTime = Time.time + fireRate;
+            UpdateAmmoUI();
+        }
+    }
+    
+    void UpdateAmmoUI()
+    {
+        ammoText.text = "Ammo: " + currentAmmo + " / " + maxMagAmmo + " | Total: " + maxAmmo;
     }
 }
